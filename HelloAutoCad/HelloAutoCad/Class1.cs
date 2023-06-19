f9014eeb43d7546541c45d143c696d87334402cc
Aditya <gadgeaditya05@gmail.com>
2023-06-19 10:15:29 +0530

    add

---
+++ HelloAutoCad/HelloAutoCad/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+
+namespace HelloAutoCad
+
+{
+    public class Class1
+    {
+        [CommandMethod("HelloAutoCad")]
+        public void HelloAutoCADFromCSharp()
+        {
+            Document document = Application.DocumentManager.MdiActiveDocument;
+            Database db = document.Database;
+           Editor   editor= document.Editor;
+
+            editor.WriteMessage("Hello AutoCAD with C# Automation.....!!!!!");
+        }
+    }
+}
